9c9a01ddb35d0d0e7de52799d4e60bc6634cdd5c
smash8tap <sm9843@rit.edu>
2022-08-25 15:02:35 -0400

    Updated the response link

--- notifier.sh
+++ notifier.sh
@@ -3,7 +3,6 @@ if [[ $? -ne 0  ]]; then
    # Ngrok not running
    nohup ngrok tcp 8006 &
    sleep 5
-   public_url=$(curl localhost:4040/api/tunnels | jq  '.tunnels' | jq '.[0].public_url' | tr -d '"')  
+   public_url=$(curl localhost:4040/api/tunnels | jq  '.tunnels' | jq '.[0].public_url' | tr -d '"' | sed "s/^tcp/https/g")  
    curl -X POST $WEBHOOK -H 'Content-Type:application/json' -d '{ "content": "'"$public_url"'"}'
-
 fi
