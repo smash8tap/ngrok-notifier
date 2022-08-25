edffc5c81a8c7cdfc966c63817999d4aa70ff43f
smash8tap <sm9843@rit.edu>
2022-08-25 09:09:59 -0400

    Script Added

---
+++ notifier.sh
@@ -0,0 +1,9 @@
+ps aux | grep -v grep  | grep ngrok 2> /dev/null 1>&2
+if [[ $? -ne 0  ]]; then
+   # Ngrok not running
+   nohup ngrok tcp 8006 &
+   sleep 5
+   public_url=$(curl localhost:4040/api/tunnels | jq  '.tunnels' | jq '.[0].public_url' | tr -d '"')  
+   curl -X POST $WEBHOOK -H 'Content-Type:application/json' -d '{ "content": "'"$public_url"'"}'
+
+fi
